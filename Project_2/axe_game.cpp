de2a3788494dfdb146e8b34c82094b4058c97874
Jiao Jian <jj.jianjiao@gmail.com>
2022-06-20 14:36:06 -0600

    Update axe_game.cpp

--- Project_2/axe_game.cpp
+++ Project_2/axe_game.cpp
@@ -6,12 +6,12 @@ int main(){
     int height = 200;
     const char* title = "My first Raylib game";
     InitWindow(width,height, title);
-    while (true)
+    
+    while (!WindowShouldClose())
     {
         BeginDrawing();
-
         ClearBackground(RED);
         EndDrawing();
     }
     
-}
+} 
