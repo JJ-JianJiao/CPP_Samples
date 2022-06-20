5048b696c2da468558c199503028f1d689c89794
Jiao Jian <jj.jianjiao@gmail.com>
2022-06-20 11:43:12 -0600

    Update Hello.cpp

--- Project_1/Hello.cpp
+++ Project_1/Hello.cpp
@@ -1,7 +1,13 @@
 #include <cstdio>
 
+/*
+*classic hello CPP
+*Prints some string datas
+*By JJ
+*/
 int main(){
     
+    //printf will print the string data
     printf("Hello, C++ see you again");
     printf("\nHello, C++. For some reasons, I did not use you for about 10 years. Today, i will pick you you again!");
     printf("\nI have no idea about my future and what I can do. I have a dream to because a game developer and I hope I can develop some AAA games.");
