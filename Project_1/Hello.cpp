33336ab90e4458d752705f06151e12e279c5dbfd
Jiao Jian <jj.jianjiao@gmail.com>
2022-06-20 11:28:53 -0600

    Initial commit

---
+++ Project_1/Hello.cpp
@@ -0,0 +1,9 @@
+#include <cstdio>
+
+int main(){
+    
+    printf("Hello, C++ see you again");
+    printf("\nHello, C++. For some reasons, I did not use you for about 10 years. Today, i will pick you you again!");
+    printf("\nI have no idea about my future and what I can do. I have a dream to because a game developer and I hope I can develop some AAA games.");
+
+}
